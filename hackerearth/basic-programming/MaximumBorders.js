399a2936f337b1413f79da14a457eb1bc7e82a08
maheshkolla1234 <49141968+maheshkolla1234@users.noreply.github.com>
2022-08-23 02:01:37 +0530

    Update MaximumBorders.js

--- hackerearth/basic-programming/MaximumBorders.js
+++ hackerearth/basic-programming/MaximumBorders.js
@@ -7,16 +7,18 @@ process.stdin.on("data", function (input) {
 });
 
 let getEveryTableMaxBackCells = (table) => {
+    // Probably two function to make it more readable ?
     return table.map(row => {
         let eachRowBlackCells =  row.map(element => {
             let blackCells = element.match(/[#]+/g);
             return blackCells ? blackCells[0].length : 0;
         });
-        return Math.max(...eachRowBlackCells);
+        return Math.max(...eachRowBlackCells); // inline this ?
     });
 };
 
 let formatOutput = (output) => {
+    // Why do we need to write this ?  why not use inbuilt stirng join function ?
     return output.reduce((result, currentValue) => {
         return result + "\n" + currentValue;
     }, "");
@@ -29,14 +31,15 @@ let formatInput = (input, tableIndex, formatted_input = []) => {
     }
 
     let newTable = inputLines[tableIndex];
-    let tableRowCount = JSON.parse(newTable.split(" ")[0]);
+    let tableRowCount = JSON.parse(newTable.split(" ")[0]); // Why JSON parse?
     let table = inputLines.slice(tableIndex + 1, tableIndex + 1 + tableRowCount);
-    formatted_input.push(table);
+    formatted_input.push(table); // Argument mutation happening and seems like recursion is an over kill for this problem
 
     return formatInput(input, tableIndex + 1 + tableRowCount, formatted_input);
 }
 
 function main() {
+    // would be good to take input as argument from outside so that this method is independent of global variable and can be tested, no side effects
     let formatted_input = formatInput(stdin_input, 1);
     let output = getEveryTableMaxBackCells(formatted_input);
     let formatted_output = formatOutput(output);
