8840509b1740de839aae590311834b45663cbdc0
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2022-08-25 12:59:53 +0530

    Regex question

--- hackerearth/basic-programming/problems/zoos/Zoos.js
+++ hackerearth/basic-programming/problems/zoos/Zoos.js
@@ -1,4 +1,5 @@
 const getOutput = (input) => {
+  // Can this be done using a single regex ? Do NOT look at my code before fixing.
   const x = input.match(/[z]+/g)[0].length;
   const y = input.match(/[o]+/g)[0].length;
   return 2*x == y ? "Yes" : "No";
