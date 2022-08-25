7480a609cdbbab25b163a7afa48df27236fabed7
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2022-08-25 13:07:45 +0530

    Update Divisibility.js

--- hackerearth/basic-programming/problems/divisibility/Divisibility.js
+++ hackerearth/basic-programming/problems/divisibility/Divisibility.js
@@ -1,4 +1,5 @@
 const getOutput = (input) => {
+  // This can be improved. Do NOT look at my code before improving.
   const numbers = input.split("\n")[1];
   return parseInt(numbers[numbers.length - 1]) == 0 ? "Yes" : "No";
 }
